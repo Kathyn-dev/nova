c2f056035a86389f4f03338fdebc8f8c813b7ac6
Katharyn <katharynsanches@gmail.com>
2026-08-21 23:42:25 -0300

    first commit

---
+++ src/main/java/com/nova/nova_assistant/NovaAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.nova.nova_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaAssistantApplication.class, args);
+	}
+
+}
